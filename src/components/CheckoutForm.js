3f2f5f91742470689b5e7e2d1a1fc8e256b25487
darkseiD779966 <123389349+darkseiD779966@users.noreply.github.com>
2023-05-12 16:44:05 +0530

    Update CheckoutForm.js

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -20,7 +20,7 @@ function CheckoutForm() {
         e.preventDefault();
         if (!stripe || !elements || user.cart.count <= 0) return;
         setPaying(true);
-        const { client_secret } = await fetch("https://ecomern-back.onrender.com/create-payment", {
+        const { client_secret } = await fetch("https://ecand.onrender.com/create-payment", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
